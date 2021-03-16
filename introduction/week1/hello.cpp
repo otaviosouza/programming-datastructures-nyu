c14c17db721cab9184516f6a5703eae4c5967317
Otavio Souza <otaviojr.work@gmail.com>
2021-03-16 08:25:28 -0300

    Week 1

---
+++ introduction/week1/hello.cpp
@@ -0,0 +1,14 @@
+/**************************************
+* hello.cpp - Week 1 Lab              *
+* It prints Hello World               *
+* and prints their sum                *
+***************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+
+	return 0;
+} // main(void)
---
+++ introduction/week1/sum.cpp
@@ -0,0 +1,25 @@
+/**************************************
+* sum.cpp - Week 1                    *
+* It reads two integers from the user *
+* and prints their sum                *
+***************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// declaration of variables
+	int num1;
+	int num2;
+	int sum;
+
+	// interaction with the user
+	cout << "Please enter two numbers separated by a space:" << endl;
+	cin >> num1 >> num2;
+
+	sum = num1 + num2;
+
+	cout << num1 << " + " << num2 << " = " << sum << endl;
+
+	return 0;
+} // main(void)
