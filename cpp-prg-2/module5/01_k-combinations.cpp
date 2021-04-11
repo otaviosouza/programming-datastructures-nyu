1e54250438fdd82a11c456bc2f37f94fbbf08bf2
Otavio Souza <otaviojr.work@gmail.com>
2021-04-11 10:11:13 -0300

    begins(Module5): Begins Module 5 Functions

---
+++ cpp-prg-2/module5/01_k-combinations.cpp
@@ -0,0 +1,16 @@
+/*************************************************
+* 01_k-combinations.cpp - Module 5               *
+* Write a program that reads from the user       *
+* two positive integers n, k (n >= k), and       *
+* prints the value of n choose k.                *
+* History:                                       *
+*   v1.0.0 2021-04-11, Otávio Souza              *
+*     - first release                            *
+*************************************************/
+
+#include <iostream>
+
+int main(void)
+{
+
+} // closes main(void)
